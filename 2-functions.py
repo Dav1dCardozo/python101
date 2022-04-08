382c9d08e475a697a6e96917613519817aef617e
David Cardozo <cardozodavid713@gmail.com>
2022-04-07 22:22:50 -0400

    homework file

--- 2-functions.py
+++ 2-functions.py
@@ -1,3 +1,4 @@
+#NAME GREETING
 def greet(first_name, last_name):
     print(f"Top of the morning to you, {first_name} {last_name}")
 
@@ -6,8 +7,17 @@ def greet(first_name, last_name):
 greet("David", "Cardozo")
 greet("Eric", "Cartman")
 
+# EMAIL GENERATOR
 def email_gen(first_name, last_name, domain):
-        print(first_name[0] + "." + last_name[0:5] + domain + ".com")
+     print(first_name[0] + "." + last_name[0:5] + domain + ".com")
 
 
 email_gen("Eric","Cartman","@starkindustries")
+
+#USERNAME 
+def user_name(first_name, last_name):
+    print(first_name[0:3] + "_" + last_name[0:5])
+
+
+user_name("Eric","Cartman")
+
